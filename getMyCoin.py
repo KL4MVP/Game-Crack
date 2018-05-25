29177aeebc62f4988610fc23da8107d3f81c379f
KL4MVP <39611251+KL4MVP@users.noreply.github.com>
2018-05-25 17:28:22 +0800

    Update getMyCoin.py

--- getMyCoin.py
+++ getMyCoin.py
@@ -6,13 +6,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import StaleElementReferenceException
 
-
+# 判断元素是否存在的函数
 # def is_element_exist(clas):
 #     s = driver.find_elements_by_class_name(clas)
 #     if len(s) == 0:
 #         return  False
 #     else:
 #         return  True
+
 
 driver = webdriver.Chrome()
 driver.get("http://t.cn/RmoVQ8t")
@@ -27,6 +28,7 @@ time.sleep(5)
 driver.find_element_by_class_name('btn_start_image').click()
 time.sleep(5)
 
+# 控制游戏局数
 j = 20
 
 while j > 0:
@@ -36,16 +38,13 @@ while j > 0:
     e3 = driver.find_element_by_xpath("//*[@id='game_body']/div[10]")
     e4 = driver.find_element_by_xpath("//*[@id='game_body']/div[11]")
 
-
-    # i = 1000
     i = True
 
     while  i:
-        # e = driver.find_element_by_class_name('game_list')
-        # if EC.presence_of_element_located((By.CLASS_NAME,'next_he_brand')):
-        # if is_element_exist('next_he_brand'):
         try:
             chain = ActionChains(driver)
+            
+#           上下左右拖动鼠标
             chain.drag_and_drop_by_offset(e1,0,-2).perform()
             # time.sleep(2)
             chain.drag_and_drop_by_offset(e2, -2, 0).perform()
@@ -55,11 +54,13 @@ while j > 0:
             chain.drag_and_drop_by_offset(e4, 2, 0).perform()
             # time.sleep(2)
             time.sleep(0.5)
-            i += 1
-            print(i)
+#             i += 1
+#             print(i)
+            
         except StaleElementReferenceException as msg:
             break
 
+#            开启新局
     time.sleep(1)
     print('NEW GAME %d' % j)
     driver.find_element_by_id('btnReset').click()
